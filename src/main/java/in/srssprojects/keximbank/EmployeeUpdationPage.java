3071bef41878adba6cbfa5e39971e2da9d72278b
TANT-A01\desma13 <desma13@ca.com>
2019-04-15 21:11:30 +0530

    Update Pages for Branch, Employee, Role

--- src/main/java/in/srssprojects/keximbank/EmployeeUpdationPage.java
+++ src/main/java/in/srssprojects/keximbank/EmployeeUpdationPage.java
@@ -1,5 +1,79 @@
 package in.srssprojects.keximbank;
 
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.How;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.Select;
+
+/*
+ * 
+ * @author -- Manikanta
+ */
 public class EmployeeUpdationPage {
+	private WebDriver driver;
+
+	// Employee Name
+	@FindBy(how = How.ID, using = "")
+	private WebElement empName;
+
+	// Login Password
+	@FindBy(how = How.ID, using = "")
+	private WebElement logPass;
+
+	// role
+	@FindBy(how = How.ID, using = "")
+	private WebElement role;
+
+	// branch
+	@FindBy(how = How.ID, using = "")
+	private WebElement branch;
+
+	// update button
+	@FindBy(how = How.ID, using = "")
+	private WebElement updateButton;
+
+	// CancelButton
+	@FindBy(how = How.ID, using = "")
+	private WebElement cancelButton;
+
+	public EmployeeUpdationPage(WebDriver driver) {
+		this.driver = driver;
+		PageFactory.initElements(driver, this);
+	}
+
+	// fill Employee Name
+	public void setEmpName(String empName) {
+		this.empName.sendKeys(empName);
+	}
+
+	// fill login Password
+	public void setPassword(String passwd) {
+		this.logPass.sendKeys(passwd);
+	}
+
+	// select role
+	public void setRole(String role) {
+		new Select(this.role).selectByVisibleText(role);
+	}
+
+	// select branch
+	public void setBranch(String branch) {
+		new Select(this.branch).selectByVisibleText(branch);
+	}
+
+	// click update Button
+	public Alert updateButton() {
+		this.updateButton.click();
+		return driver.switchTo().alert();
+	}
+
+	public EmployeeDetailsPage cancelButton() {
+		this.cancelButton.click();
+		return PageFactory.initElements(driver, EmployeeDetailsPage.class);
+
+	}
 
 }
--- src/main/java/in/srssprojects/keximbank/RoleUpdationPage.java
+++ src/main/java/in/srssprojects/keximbank/RoleUpdationPage.java
@@ -1,5 +1,66 @@
 package in.srssprojects.keximbank;
 
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.How;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.Select;
+
+
+/*
+ * 
+ * @author -- Manikanta
+ */
 public class RoleUpdationPage {
 
+	private WebDriver driver;
+
+	// roleName
+	@FindBy(how = How.ID, using = "txtrNameU")
+	private WebElement roleName;
+
+	// role description
+	@FindBy(how = How.ID, using = "txtrdescU")
+	private WebElement roleDesc;
+
+	// role type
+	@FindBy(how = How.ID, using = "lstRtype")
+	private WebElement roleType;
+
+	// update button
+	@FindBy(how = How.ID, using = "btnupdate")
+	private WebElement update;
+
+	// cancel button
+	@FindBy(how = How.ID, using = "Btncancel")
+	private WebElement cancel;
+
+	// fill roleName
+	public void setRoleName(String roleName) {
+		this.roleName.sendKeys(roleName);
+	}
+
+	// fill role description
+	public void setRoleDesc(String roleDesc) {
+		this.roleDesc.sendKeys(roleDesc);
+	}
+
+	// select roleType
+	public void selectRoleType(String roleType) {
+		new Select(this.roleType).selectByVisibleText(roleType);
+	}
+
+	// click update button
+	public Alert clickUpdate() {
+		this.update.click();
+		return driver.switchTo().alert();
+	}
+
+	// click cancel button
+	public RoleDetailsPage clickCancel() {
+		this.cancel.click();
+		return PageFactory.initElements(driver, RoleDetailsPage.class);
+	}
 }
